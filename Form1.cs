cc6c4eab85e4ff0a0ec2f3c376a265b2938726b9
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-07 11:38:58 +0300

    kodlarim

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stack_form_tasarımı_uygulaması_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stack yigin = new Stack();
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            yigin.Push(txtAdSoyad.Text);
+            Listele();
+        }
+        private void Listele()
+        {
+            listeStack.Items.Clear();
+            foreach (string eleman in yigin)
+            {
+                listeStack.Items.Add(eleman);
+            }
+        }
+
+        private void btnCikart_Click(object sender, EventArgs e)
+        {
+            yigin.Pop();
+            Listele();
+        }
+    }
+}
